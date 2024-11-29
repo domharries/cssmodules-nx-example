7232a1d94435cb80e8f98efea9d2346ada4abb73
Dominic Harries <dom@h5s.org>
2024-11-29 13:59:24 +0000

    Initial commit

---
+++ apps/cssmodules-nx-example/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'cssmodules-nx-example',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/cssmodules-nx-example',
+};
