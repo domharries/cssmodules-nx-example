9a18399fba1aa0f06927541667e64eed331ef2c4
Dominic Harries <dom@h5s.org>
2024-12-01 21:16:50 +0000

    Add styles project with util css

--- apps/cssmodules-nx-example/src/app/app.tsx
+++ apps/cssmodules-nx-example/src/app/app.tsx
@@ -1,10 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
+import util from '@cssmodules-nx-example/styles/util.module.css';
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <div>
+      <p className={styles.foo}>testing 123</p>
+      <p className={util.imported}>testing 123</p>
       <NxWelcome title="cssmodules-nx-example" />
     </div>
   );
